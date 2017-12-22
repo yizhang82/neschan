e45e704f3cf4cb91a2067b6c968ccbaf2679bd19
Yi Zhang <yizhang82@outlook.com>
2017-12-21 23:18:36 -0800

    Adjust cycle/ms calculation

--- lib/inc/nes_cycle.h
+++ lib/inc/nes_cycle.h
@@ -6,6 +6,13 @@
 using namespace std::chrono;
 
 //
+// NES 6502 CPU 21.477272 / 12 MHz
+// NES PPU 21.477272 / 4 MHz
+// @TODO - /4 is too fast so use /12. need to double check the math/facts
+//
+#define NES_CLOCK_HZ (21477272ul / 12)
+
+//
 // Given that 1 CPU cycle = 3 PPU cycle, we'll count in terms of PPU cycle
 // 1 nes_cycle_t = 1 PPU cycle = 1/3 CPU cycle
 // 3 nes_cycle_t = 3 PPU cycle = 1 CPU cycle
@@ -13,5 +20,10 @@ using namespace std::chrono;
 typedef duration<int64_t, std::ratio<1, 1>> nes_cycle_t;
 typedef duration<int64_t, std::ratio<1, 1>> nes_ppu_cycle_t;
 typedef duration<int64_t, std::ratio<3, 1>> nes_cpu_cycle_t;
+
+static nes_cycle_t ms_to_nes_cycle(uint32_t ms)
+{
+    return nes_cycle_t(NES_CLOCK_HZ * ms / 1000);
+}
 
 #define PPU_SCANLINE_CYCLE nes_ppu_cycle_t(341)  
